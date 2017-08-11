b8cb7fa41f2bead2ffb9dd78199d568284ddf18e
JoseArciniegas <arciniegas2012@utp.edu.cogit config --globalgit config --global user.email arciniegas2012@utp.edu.co>
2017-08-11 13:49:46 -0500

    Estoy subiendo un archivo en Ruby

---
+++ HolaMundo.rb
@@ -0,0 +1,12 @@
+class HolaMundo
+	def	initialize()
+	end
+	def saluda()
+		nombre = "Hola Mundo"
+		puts nombre
+	end
+end
+# Creamos el objeto de la clase Hola Mundo
+objeto = HolaMundo.new()
+objeto.saluda
+gets()
